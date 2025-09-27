eec4a6cce50605c65eae2b8dac061dcaaed2939b
nolan <venqoi@protonmail.com>
2025-09-27 16:31:44 -0400

    Fixed cors error for pringles, and I guess everyone else

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import Elysia from "elysia";
+import cors from "@elysiajs/cors";
 import mainController from "./routes/main";
 import { respond } from "./lib/respond";
 
 export const app = new Elysia({ aot: false })
+  .use(cors({ origin: "*" }))
   .onError(({ code, error }: any) => {
     return respond(error.status, {
       success: false,
